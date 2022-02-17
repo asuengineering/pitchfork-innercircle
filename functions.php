26c72c18a3a9c2ff7b4c28dc0142e6b699cdd495
Steve Ryan <steve.ryan@asu.edu>
2022-02-17 15:34:24 -0700

    Remove code pertaining to GF intake form.

--- functions.php
+++ functions.php
@@ -49,7 +49,6 @@ require get_stylesheet_directory() . '/inc/font-awesome-pro.php';
 require get_stylesheet_directory() . '/inc/event-line.php';
 require get_stylesheet_directory() . '/inc/uds-calendar-dates.php';
 require get_stylesheet_directory() . '/inc/rest-api-extensions.php';
-require get_stylesheet_directory() . '/inc/gravityforms.php';
 
 /** 
  * Pull just the categories from a post.
