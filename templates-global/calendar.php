0fddc7dbf50d01cbab3ca4c0a5e208743787320c
Steve Ryan <steve.ryan@asu.edu>
2024-02-29 17:09:08 -0700

    Add correct path to assets for enqueue

--- templates-global/calendar.php
+++ templates-global/calendar.php
@@ -140,8 +140,8 @@ function pass_events_to_fullcalendar() {
     // Get the theme data.
 	$the_theme     = wp_get_theme();
 	$theme_version = $the_theme->get( 'Version' );
-    $js_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/js/fullcalendar-init.js' );
-	wp_enqueue_script( 'pitchfork-innercircle-fullcalendar-init', get_stylesheet_directory_uri() . '/js/fullcalendar-init.js', array( 'pitchfork-fullcalendar' ), $js_child_version );
+    $js_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/dist/js/fullcalendar-init.js' );
+	wp_enqueue_script( 'pitchfork-innercircle-fullcalendar-init', get_stylesheet_directory_uri() . '/dist/js/fullcalendar-init.js', array( 'pitchfork-fullcalendar' ), $js_child_version );
     wp_add_inline_script( 'pitchfork-innercircle-fullcalendar-init', 'const CALDATA = ' . json_encode( array(
         'events' => $event_array,
     ) ), 'before' );
